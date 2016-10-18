cb3e1cc7e9882b2392acefbee55a36cc14b8d554
ndrewnee <ndrewnee@gmail.com>
2016-10-18 14:15:39 +0500

    tests fixed

--- sqlreader_test.go
+++ sqlreader_test.go
@@ -18,23 +18,15 @@ func TestSqlReader_Get(t *testing.T) {
 		{"not_found", ""},
 		{"select", "SELECT * FROM test_table WHERE id = ?"},
 		{"outer/update", "UPDATE test_table SET test = TRUE"},
-		{"outer/inner\\insert", "INSERT INTO some_table(name) VALUES(:name)"},
+		{"outer/inner/insert", "INSERT INTO some_table(name) VALUES(:name)"},
 		{"dir", ""},
 	}
 
-	f := func(key, expected string) {
-		for i := 0; i < 1000; i++ {
-			sql := s.Get(key)
-
-			if sql != expected {
-				t.Errorf("Expected %s, got %s\n", expected, sql)
-			}
-		}
-	}
-
 	for _, v := range asserts {
-		// Concurrency testing
-		go f(v.key, v.expected)
-		go f(v.key, v.expected)
+		sql := s.Get(v.key)
+
+		if v.expected != sql {
+			t.Errorf("Expected %s, got %s\n", v.expected, sql)
+		}
 	}
 }
